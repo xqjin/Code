505caf388be095ffe2f0aa0e42fca1deea211bba
”xqjin“ <jinxiaoqiang0608@gmail.com>
2016-03-24 12:23:07 +0800

    add command to Shell

--- LeetCode/BinaryTreeMaximumPathSum.cpp
+++ LeetCode/BinaryTreeMaximumPathSum.cpp
@@ -1,8 +1,6 @@
 Given a binary tree, find the maximum path sum.
 The path may start and end at any node in the tree.
 
-
-
 /**
  * Definition for binary tree
  * struct TreeNode {
---
+++ Shell/common.sh
@@ -0,0 +1,4 @@
+#paste [-s] [-d <separetor>] [--help]
+# -s:  col-contate
+
+paste -d '*' 1.txt 2.txt > res.txt
