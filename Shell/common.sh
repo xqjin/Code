da2c2b3c4eff6890079e2a84841f6cd6a26db2ee
”xqjin“ <jinxiaoqiang0608@gmail.com>
2016-04-11 11:28:01 +0800

    add into common.sh

--- Shell/common.sh
+++ Shell/common.sh
@@ -1,13 +1,19 @@
 #paste [-s] [-d <separetor>] [--help]
 # -s:  col-contate
-
-paste -d '*' 1.txt 2.txt > res.txt
+#example
+#paste -d '*' 1.txt 2.txt > res.txt
 
 #hostname 
 #output the hostname of the server
 
-
 #host hostname 
 #show the IP of the host
+
+#here document
+(
+cat << EOF
+content we want to write to output_file
+EOF
+) >> output_file
 
 
