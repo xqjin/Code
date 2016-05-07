7d674e8b2e2f75c24adff5767aaebd9be6c4d504
”xqjin“ <jinxiaoqiang0608@gmail.com>
2016-05-07 12:27:17 +0800

    modify common.sh

--- Shell/common.sh
+++ Shell/common.sh
@@ -1,19 +1,41 @@
-#paste [-s] [-d <separetor>] [--help]
+# paste [-s] [-d <separetor>] [--help]
 # -s:  col-contate
-#example
-#paste -d '*' 1.txt 2.txt > res.txt
+# example
+# paste -d '*' 1.txt 2.txt > res.txt
 
-#hostname 
-#output the hostname of the server
+# hostname 
+# output the hostname of the server
 
-#host hostname 
-#show the IP of the host
+# host hostname 
+# show the IP of the host
 
-#here document
+# here document
 (
 cat << EOF
-content we want to write to output_file
+"content we want to write to output_file"
 EOF
 ) >> output_file
 
+# how to judge the condition
+# if the condition is ture, echo yes
+[ condition ] && echo yes 
+test condition && echo yes 
 
+# replace the string
+old_string="old_jinxiaoqiang_old"
+new_string=${old_string/old/new} //replace the first old
+new_string=${old_string//old/new} //replace all the old
+
+# string cut
+string="jinxiaoqiang"
+new_string=${string:begin:end} # string[begin:end) begin = 0,1,2..
+
+# cut the path of the file
+basename dirname # get filename or directory name
+
+# string delete
+${string#pattern}
+${string##pattern}
+
+${string%pattern}
+${string%%pattern}
