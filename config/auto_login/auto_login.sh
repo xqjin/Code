802d302b5b411eaa080e0b495edc340b42f3b292
”xqjin“ <jinxiaoqiang0608@gmail.com>
2016-05-28 12:04:49 +0800

    config used in work

---
+++ config/auto_login/auto_login.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/expect
+if { $argc != 2 } {
+    send_user "Usage: user@host passwd\n"
+    exit
+}
+
+set user_host [lindex $argv 0]
+set passwd [lindex $argv 1]
+
+spawn /usr/local/bin/luit -encoding gb18030 ssh baidu
+expect "*ssl*" {send "ssh $user_host \r"}
+expect "*password:*"
+send "$passwd\r"
+
+#fconfigure stdout -encoding ascii
+#fconfigure stdout -encoding cp936
+#fconfigure stdin  -translation binary
+#fconfigure stdout -translation binary
+
+#fconfigure stdin -translation binary
+#fconfigure stdin -encoding cp936
+
+#fconfigure stdin -encoding gb2312
+#fconfigure stdout -encoding gb2312
+
+#send "export LANG=en_US.UTF-8;export LC_ALL=en_US.UTF8;export LC_CTYPE=en_US.UTF8\r"
+#send "export LANG=zh_CN.gbk;export LC_ALL=zh_CN.gbk;export LC_CTYPE=zh_CN.gbk\r"
+#send "export LANG=zh_CN.GB18030;export LC_ALL=zh_CN.GB18030;export LC_CTYPE=zh_CN.GB18030\r"
+
+interact
