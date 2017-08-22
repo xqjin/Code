aa2dd9520d11eae33810126d5859600c04915c7d
novice <jinxiaoqiang0608@gmail.com>
2017-08-22 10:02:51 +0800

    Make STL demo

---
+++ C++/stl/main.cpp
@@ -0,0 +1,11 @@
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<iostream>
+
+using namespace::std;
+
+int main(int argc, char ** argv) {
+    cout << argc << endl; 
+    return 0;
+}
